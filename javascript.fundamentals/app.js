8b7fb3347355dc19ba4f641b229d78f7568253b5
xyz <anushkagupta200518@gmail.com>
2026-04-06 19:53:24 +0530

    initial commit

---
+++ javascript.fundamentals/app.js
@@ -0,0 +1,22 @@
+// var name= "Anushka";
+// var age= 30;
+// var isEmployed= true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// var age=50;
+// console.log(age);
+
+// let name= "Anushka";
+// let age= 30;
+// let isEmployed= true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// let age=50;
+// console.log(age);
+
+let name= "Anushka";
+let age1= 19;
+console.log("my name is" +name+ "and my age is" +age1+);
+console>log(`my name is`+${name} "and my age is" +${age1});
